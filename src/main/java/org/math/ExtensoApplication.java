cc8e39bf63d5eed2ec1756e32fbabe3217687b72
Thiago Bomfim <thiagobomfim1995@gmail.com>
2020-04-13 18:34:25 -0300

    Criando o projeto

---
+++ src/main/java/org/math/ExtensoApplication.java
@@ -0,0 +1,14 @@
+package org.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtensoApplication.class, args);
+	}
+
+}
+
